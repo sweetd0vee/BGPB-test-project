902e8874a70d24f397ab77d93e33cecc3b1f2b43
Arina Goloubitskaya <arinaogo@gmail.com>
2025-09-26 18:00:24 +0300

    added first task

---
+++ 1. SQL task/task-1.sql
@@ -0,0 +1,16 @@
+select
+	l.num_loan,
+	l.risk_group,
+	case 
+		when l.code_curr = '840' then 'Доллары США'
+		when l.code_curr = '978' then 'Евро'
+	end as currency,
+	lf.rest_od_eq + lf.rest_pd_eq as rest_eq
+from
+	LOANS as l
+join LOANS_FACT as lf on
+	l.id_loan = lf.id_loan
+where
+	l.dt_end = '2023-09-30'
+	and (l.code_curr = '840'
+		or l.code_curr = '978')
