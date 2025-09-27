0d7e29c56ad83d7cac6d9096bb72b021e0d47f29
Arina Goloubitskaya <155384491+sweetd0vee@users.noreply.github.com>
2025-09-27 15:35:56 +0300

    Update create_tables.sql

--- 1. SQL task/create_tables.sql
+++ 1. SQL task/create_tables.sql
@@ -1,31 +1,31 @@
 create table clients (
-    id_client INT not null,
-    dt_start DATE not null,
-    dt_end DATE null,
-    name_client VARCHAR(255) not null,
-    type_client VARCHAR(20) null,
-    department VARCHAR(20) null,
+    id_client int not null,
+    dt_start date not null,
+    dt_end date null,
+    name_client varchar(255) not null,
+    type_client varchar(20) null,
+    department varchar(20) null,
 
     primary key (id_client, dt_start) 
 );
 
 create table loans (
-    id_loan INT not null,
-    dt_start DATE not null,
-    dt_end DATE not null,
-    id_client INT,
-    num_loan VARCHAR(20) not null,
-    dt_open_loan DATE null,
-    code_curr VARCHAR(3),
+    id_loan int not null,
+    dt_start date not null,
+    dt_end date not null,
+    id_client int,
+    num_loan varchar(20) not null,
+    dt_open_loan date null,
+    code_curr varchar(3),
     int_rate decimal(4, 2) not null,
-    risk_group VARCHAR(20) null,
+    risk_group varchar(20) null,
     
     primary key (id_loan, dt_start)
 );
 
 create table loans_fact (
-    id_loan INT primary key,
-    dt DATE not null,
+    id_loan int primary key,
+    dt date not null,
    	rest_od decimal(10, 2) null,
    	rest_od_eq decimal(10, 2) null,
    	rest_pd decimal(10, 2) null,
