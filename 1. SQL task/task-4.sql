ef40a314a76e8693a734c7851e543f68b9851bdd
Arina Goloubitskaya <155384491+sweetd0vee@users.noreply.github.com>
2025-09-27 16:22:00 +0300

    Update task-4.sql

--- 1. SQL task/task-4.sql
+++ 1. SQL task/task-4.sql
@@ -1,4 +1,18 @@
-select name_client
-from clients
-where type_client = 'ЮЛ' and LOWER(name_client) like 'ооо%'
--- в 2022 году было открыто более 1 договора
+select
+    name_client
+from
+    (
+        select
+            name_client,
+            count(l.id_loan) as num_loans
+        from
+            clients c
+        join
+            loans l ON c.id_client = l.id_client
+        where
+            type_client = 'ЮЛ' and lower(name_client) like 'ооо%' -- Фильтрация по типу и наименованию клиента
+            and dt_open_loan between '2022-01-01' and '2022-12-31' -- Фильтрация по дате договора
+        group by
+            c.name_client
+        having count(l.id_loan) > 1 -- Фильтрация по количеству договоров
+    ) as filtered_clients;
