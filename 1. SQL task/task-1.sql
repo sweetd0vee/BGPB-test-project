507d1a56648bd08e6871967a1d5b0bd66fc893e1
Arina Goloubitskaya <155384491+sweetd0vee@users.noreply.github.com>
2025-09-28 18:05:14 +0300

    Update task-1.sql

--- 1. SQL task/task-1.sql
+++ 1. SQL task/task-1.sql
@@ -1,16 +1,16 @@
-select
+select 
 	l.num_loan,
 	l.risk_group,
 	case 
 		when l.code_curr = '840' then 'Доллары США'
 		when l.code_curr = '978' then 'Евро'
 	end as currency,
-	lf.rest_od_eq + lf.rest_pd_eq as rest_eq -- not null
+	coalesce(lf.rest_od_eq, 0) + coalesce(lf.rest_pd_eq, 0) as rest_eq
 from
-	LOANS as l
-join LOANS_FACT as lf on
+	loans as l
+left join loans_fact as lf on
 	l.id_loan = lf.id_loan
 where
 	lf.dt = '2023-09-30'
-	and (l.code_curr = '840'
-		or l.code_curr = '978')
+	and (l.code_curr = '840' or l.code_curr = '978')
+	and coalesce(lf.rest_od_eq, 0) + coalesce(lf.rest_pd_eq, 0) != 0
