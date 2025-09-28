148e4eeff6cc729eedbe273a12f22f6f29d091e7
Arina Goloubitskaya <155384491+sweetd0vee@users.noreply.github.com>
2025-09-28 20:21:26 +0300

    Update SQL-task.sql

--- 1. SQL task/SQL-task.sql
+++ 1. SQL task/SQL-task.sql
@@ -66,22 +66,22 @@ INSERT INTO loans_fact values
 
 ------- task 1 ------------------------------------------------------------------
 
-select
+select 
 	l.num_loan,
 	l.risk_group,
 	case 
 		when l.code_curr = '840' then 'Доллары США'
 		when l.code_curr = '978' then 'Евро'
 	end as currency,
-	lf.rest_od_eq + lf.rest_pd_eq as rest_eq -- not null
+	coalesce(lf.rest_od_eq, 0) + coalesce(lf.rest_pd_eq, 0) as rest_eq
 from
 	loans as l
-join loans_fact as lf on
+left join loans_fact as lf on
 	l.id_loan = lf.id_loan
 where
 	lf.dt = '2023-09-30'
-	and (l.code_curr = '840'
-		or l.code_curr = '978')
+	and (l.code_curr = '840' or l.code_curr = '978')
+	and coalesce(lf.rest_od_eq, 0) + coalesce(lf.rest_pd_eq, 0) != 0
 
 ------- task 2 ------------------------------------------------------------------
 
